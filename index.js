cc051eb981b5b6ca73ec18b193d9bec4dbbc1f75
nietw23 <wijayanti.nitya@gmail.com>
2017-12-21 16:49:45 +0700

    add regex

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const token = 'EAAEWs25cLwoBADsVDl5a5D0SWvkdgAfupZCxSRP4VjMZBlDZCDGhkzDunPuSWmKPZA49LqkspZAtOdUNRmkho7Vy2ycEjBRLr4ZASf2zlKb3ZAYJgBbZAw7eBnZCoGtUJ6ZAJHIPcTgoc87mHZB8dTizp4CRMqt1FHH9eZCiZBijvbKYNyQZDZD'
+const crypto = require('crypto')
+const AppSecret = 'APP_YOUR_SECRET'
 
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json({verify: verifyRequestSignature}))
 
 // Process application/json
 app.use(bodyParser.json())
@@ -19,36 +21,79 @@ app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot..')
 })
 
-function sendTextMessage (sender, text) {
-  let messageData = { text: text }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: messageData
+function verifyRequestSignature (req, res, buf) {
+  let signature = req.headers['x-hub-signature']
+
+  if (!signature) {
+    console.error('You dont have signature')
+  } else {
+    let element = signature.split('=')
+    let signatureHash = element[1]
+    let expectedHash = crypto.createHmac('sha1', AppSecret).update(buf).digest('hex')
+
+    console.log('signatureHash = ', signatureHash)
+    console.log('expectedHash = ', expectedHash)
+    if (signatureHash !== expectedHash) {
+      console.error('signature invalid, send message to email or save as log')
     }
-  }, function (error, response, body) {
-    if (error) {
-      console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
+  }
+}
+
+function sendTextMessage (sender, text) {
+  let url = `https://graph.facebook.com/v2.6/${sender}?fields=first_name,last_name,profile_pic&access_token=${token}`
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let parseData = JSON.parse(body)
+      let messageData = {
+        text
+        // text: `Hi ${parseData.first_name} ${parseData.last_name}, you send message : ${text}`
+        // text: `Hi ${parseData.first_name} ${parseData.last_name}, please enter your JNE tracking number with format 'jne_<tracking number>'`
+      }
+      request({
+        url: 'https://graph.facebook.com/v2.10/me/messages',
+        qs: {
+          access_token: token
+        },
+        method: 'POST',
+        json: {
+          recipient: {
+            id: sender
+          },
+          message: messageData
+        }
+      }, function (error, response, body) {
+        if (error) {
+          console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+        }
+      })
     }
   })
 }
 
+function checkUserInput (str) {
+  // jne_1234567890
+  const pattern = /^jne_\w*?$/i
+  return pattern.test(str)
+}
+
 app.post('/webhook/', function (req, res) {
-  let messagingEvents = req.body.entry[0].messaging
-  for (let i = 0; i < messagingEvents.length; i++) {
-    let event = req.body.entry[0].messaging[i]
-    let sender = event.sender.id
-    if (event.message && event.message.text) {
-      let text = event.message.text
-      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
-    }
+  let data = req.body
+  if (data.object === 'page') {
+    data.entry.forEach(function (pageEntry) {
+      pageEntry.messaging.forEach(function (messagingEvent) {
+        console.log(messagingEvent)
+        if (checkUserInput(messagingEvent.message.text)) {
+          sendTextMessage(messagingEvent.sender.id, 'Memeriksa paket anda')
+        } else {
+          sendTextMessage(messagingEvent.sender.id, 'Format salah')
+        }
+      })
+    })
+    res.sendStatus(200)
   }
-  res.sendStatus(200)
 })
 
 // for Facebook verification
